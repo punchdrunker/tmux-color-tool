f448f8c965321ad07b1959fc4dff6f91441f9413
punchdrunker <nanao.hhk@gmail.com>
2020-03-30 02:04:42 +0900

    place color palette

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,80 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div class="container">
+    <h3>tmux color selector</h3>
+    <div class="preview" :style="{ background: color1 }">
+      <div class="status_bar" :style="{ background: color2, color: color3 }">
+        <span>0:zsh 1:zsh 2:zsh</span>
+      </div>
+    </div>
+    <textarea name="kanso" rows="4" cols="40" v-model="tmuxConfig"></textarea>
+    <div class="form-group">
+      <label class="col-md-2 control-label">terminal background</label>
+      <div class="col-md-10">
+        <input class="form-control" v-model="color1" />
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-md-offset-2 col-md-10">
+        <input class="form-control" type="color" v-model="color1" />
+      </div>
+    </div>
+    <div class="color-palette">
+      <h3>Status background</h3>
+      <div
+        class="color-item"
+        v-for="color in colors"
+        :key="color.id"
+        @click="onClick2(color.rgb)"
+        :style="{ background: color.rgb }"
+      />
+      <h3>Status foreground</h3>
+      <div
+        class="color-item"
+        v-for="color in colors"
+        :key="color.id"
+        @click="onClick3(color.rgb)"
+        :style="{ background: color.rgb }"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
+
+  color1 = '#000000';
+  color2 = '#ffffff';
+  color3 = '#000000';
+  colors = [
+    { name: 'color0', rgb: '#000000' },
+    { name: 'color1', rgb: '#800000' },
+    { name: 'color2', rgb: '#008000' },
+    { name: 'color3', rgb: '#808000' },
+    { name: 'color4', rgb: '#000080' },
+    { name: 'color5', rgb: '#800080' },
+    { name: 'color6', rgb: '#008080' },
+    { name: 'color7', rgb: '#c0c0c0' }
+  ];
+
+  tmuxConfig = 'set';
+
+  public onClick2(color: string) {
+    this.color2 = color;
+    this.updateConfig();
+  }
+
+  public onClick3(color: string) {
+    this.color3 = color;
+    this.updateConfig();
+  }
+
+  public updateConfig() {
+    this.tmuxConfig = this.color2 + this.color3;
+  }
 }
 </script>
 
@@ -126,5 +93,30 @@ li {
 }
 a {
   color: #42b983;
+}
+.preview {
+  height: 100px;
+  width: 300px;
+  position: relative;
+  border-color: black;
+  border-width: 1px;
+  border-style: solid;
+}
+.status_bar {
+  height: 30px;
+  width: 284px;
+  font-weight: bold;
+  font-size: 1.2em;
+  text-align: left;
+  padding-left: 16px;
+  position: absolute;
+  bottom: 0;
+}
+.color-item {
+  height: 30px;
+  width: 30px;
+  border-color: black;
+  border-width: 1px;
+  border-style: solid;
 }
 </style>
